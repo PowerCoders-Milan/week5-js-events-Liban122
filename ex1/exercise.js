909589e8ca784399f68f16e7f28338a2f325b986
liban <botanlee515@gmail.com>
2021-03-04 13:14:10 +0100

    Done

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,23 @@
 // define your functions here
-
+var bold = document.querySelectorAll("strong");
+console.log(bold)
+var a = document.querySelector('a');
 // call the functions and connect them to an event
+a.addEventListener('mouseenter', function bo(){
+   for(i = 0; i <= bold.length; i++){
+       bold[i].style.color = 'grey'
+       bold[i].style.fontWeight = 'bold'
+   }
+})
+
+a.addEventListener('mouseleave', function bo(){
+    for(i = 0; i <= bold.length; i++){
+        bold[i].style.color = 'blue'
+        bold[i].style.fontWeight = 'normal'
+    }
+ })
+
+   
+
+
+
